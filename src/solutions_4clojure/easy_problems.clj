09d826e5baf142de5568c60678f1745ab48f9233
josby <oleh.novikov@gmail.com>
2017-02-23 23:55:05 +0100

    solved #20 - Penultimate Element

--- src/solutions_4clojure/easy_problems.clj
+++ src/solutions_4clojure/easy_problems.clj
@@ -1,6 +1,5 @@
 (ns solutions-4clojure.easy-problems)
 
-
 ; 19 - Last Element
 ; https://www.4clojure.com/problem/19
 ;
@@ -11,3 +10,13 @@
 ;(= (__ ["b" "c" "d"]) "d")
 (defn last-element [xs]
   ((comp first reverse) xs))
+
+; 20 - Penultimate Element
+; https://www.4clojure.com/problem/20
+;
+; Write a function which returns the second to last element from a sequence.
+;(= (__ (list 1 2 3 4 5)) 4)
+;(= (__ ["a" "b" "c"]) "b")
+;(= (__ [[1 2] [3 4]]) [1 2])
+(defn penultimate-element [xs]
+  ((comp second reverse) xs))

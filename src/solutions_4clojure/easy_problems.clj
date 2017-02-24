ffa6fb8b7b2afa54e69e13c63c4d7b87d85c0e95
josby <oleh.novikov@gmail.com>
2017-02-25 00:07:34 +0100

    renamed reverse-a-sequence => reverse-seq

--- src/solutions_4clojure/easy_problems.clj
+++ src/solutions_4clojure/easy_problems.clj
@@ -63,7 +63,7 @@
 ; (= (__ [1 2 3 4 5]) [5 4 3 2 1])
 ; (= (__ (sorted-set 5 7 2 7)) '(7 5 2))
 ; (= (__ [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])
-(defn reverse-a-sequence [xs]
+(defn reverse-seq [xs]
   (reduce (fn [acc, current] (conj acc current)) '() xs))
 
 
--- test/solutions_4clojure/easy_problems_tests.clj
+++ test/solutions_4clojure/easy_problems_tests.clj
@@ -25,10 +25,10 @@
   (is (= (count-a-sequence '(13)) 1))
   (is (= (count-a-sequence '(:a :b :c)) 3)))
 
-(deftest _reverse-a-sequence
-  (is (= (reverse-a-sequence [1 2 3 4 5]) [5 4 3 2 1]))
-  (is (= (reverse-a-sequence (sorted-set 5 7 2 7)) '(7 5 2)))
-  (is (= (reverse-a-sequence [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])))
+(deftest _reverse-seq
+  (is (= (reverse-seq [1 2 3 4 5]) [5 4 3 2 1]))
+  (is (= (reverse-seq (sorted-set 5 7 2 7)) '(7 5 2)))
+  (is (= (reverse-seq [[1 2][3 4][5 6]]) [[5 6][3 4][1 2]])))
 
 (deftest _sum
   (is (= (sum [1 2 3]) 6))
